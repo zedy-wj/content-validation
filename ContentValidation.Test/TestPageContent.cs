046c67acac0670c956dd465280554c94251ec9d0
Qishun Zhong (WICRESOFT NORTH AMERICA LTD) <v-qzhong@microsoft.com>
2024-11-15 16:20:54 +0800

    Add DuplicateService testcase

--- ContentValidation.Test/TestPageContent.cs
+++ ContentValidation.Test/TestPageContent.cs
@@ -10,7 +10,13 @@ namespace ContentValidation.Test
 
         static TestPageContent()
         {
-            TestLinks = JsonSerializer.Deserialize<List<string>>(File.ReadAllText("../../../appsettings.json")) ?? new List<string>();
+            //TestLinks = JsonSerializer.Deserialize<List<string>>(File.ReadAllText("../../../appsettings.json")) ?? new List<string>();
+            TestLinks = new List<string>()
+            {
+                "https://learn.microsoft.com/en-us/python/api/overview/azure/alerts-management?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/overview/azure/app-platform?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/overview/azure/automanage?view=azure-python"
+            };
         }
 
         [Test]
@@ -24,6 +30,20 @@ namespace ContentValidation.Test
             var res = await Validation.Validate(testLink);
 
             Assert.That(res.Result, testLink + " has table is empty.");
+
+        }
+        
+        [Test]
+        [TestCaseSource(nameof(TestLinks))]
+        public async Task TestDuplicateService(string testLink)
+        {
+            var playwright = await Playwright.CreateAsync();
+
+            IValidation Validation = new DuplicateServiceValidation(playwright);
+
+            var res = await Validation.Validate(testLink);
+
+            Assert.That(res.Result, res.ErrorMsg);
 
         }
     }
