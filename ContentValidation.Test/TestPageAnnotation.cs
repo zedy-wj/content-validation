fee41d73a67c8e740d1bfef9e3cd0f2ceecbece0
v-weiyding <v-weiyding@microsoft.com>
2024-11-18 17:18:00 +0800

    update testlinks

--- ContentValidation.Test/TestPageAnnotation.cs
+++ ContentValidation.Test/TestPageAnnotation.cs
@@ -10,7 +10,8 @@ namespace ContentValidation.Test
 
         static TestPageAnnotation()
         {
-            TestLinks = JsonSerializer.Deserialize<List<string>>(File.ReadAllText("../../../appsettings.json")) ?? new List<string>();
+            //TestLinks = JsonSerializer.Deserialize<List<string>>(File.ReadAllText("../../../appsettings.json")) ?? new List<string>();
+            TestLinks = new List<string> { "https://learn.microsoft.com/en-us/python/api/azure-batch/azure.batch.operations.accountoperations?view=azure-python" };
         }
 
         [Test]
