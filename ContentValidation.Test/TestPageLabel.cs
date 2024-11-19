094aaf6eeeb3338aac7b7fd83e59235791a0b150
Jianing Wang (MSFT) <141212663+jianingwang123@users.noreply.github.com>
2024-11-19 14:43:03 +0800

    Develop script `TestPageLabel.cs` and `UnnecessarySymbolsValidation.cs` (#4)
    * fix
    
    * add\n
    
    * Update ExtraLabelValidation.cs
    
    * Update ExtraLabelValidation.cs
    
    * 1

--- ContentValidation.Test/TestPageLabel.cs
+++ ContentValidation.Test/TestPageLabel.cs
@@ -25,6 +25,20 @@ namespace ContentValidation.Test
 
             Assert.That(res.Result, testLink + " has extra label \n\n" + res.Display());
         }
+
+        [Test]
+        [TestCaseSource(nameof(TestLinks))]
+        public async Task TestUnnecessarySymbols(string testLink)
+        {
+            var playwright = await Playwright.CreateAsync();
+
+            IValidation Validation = new UnnecessarySymbolsValidation(playwright);
+
+            var res = await Validation.Validate(testLink);
+
+            Assert.That(res.Result, testLink + " has unnecessary symbols:\n  " + res.ErrorMsg);
+
+        }
     }
 }
 
