b57234fa270574119a2e1d1a5296f1144a7db3bf
Weiyang Ding(MSFT) <155502284+v-weiyding@users.noreply.github.com>
2024-11-19 13:20:29 +0800

    Develop script for `Incorrectly parsed HTML elements` and `Missing type annotations` Issues (#2)
    * dev label.cs and type.cs
    
    * Update
    
    * Remove extra space
    
    * delete github.io code
    
    * update testlinks
    
    * update testlinks

---
+++ ContentValidation/MissingTypeAnnotation.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.Playwright;
+using System.Text.RegularExpressions;
+
+namespace UtilityLibraries;
+
+public class MissingTypeAnnotation : IValidation
+{
+    private IPlaywright _playwright;
+
+
+    public MissingTypeAnnotation(IPlaywright playwright)
+    {
+        _playwright = playwright;
+    }
+    public async Task<TResult> Validate(string testLink)
+    {
+        var res = new TResult();
+
+        var browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync(testLink);
+
+        Dictionary<string, List<string>>? pyClassParamMap = null;
+        Dictionary<string, List<string>>? pyMethodParamMap = null;
+
+        pyClassParamMap = await GetParamMap(page, true);
+        pyMethodParamMap = await GetParamMap(page, false);
+
+        ValidParamMap(pyClassParamMap!, true, res);
+        ValidParamMap(pyMethodParamMap!, false, res);
+
+        if (!res.IsEmpty())
+        {
+            res.Result = false;
+        }
+
+        await browser.CloseAsync();
+
+        return res;
+    }
+
+    bool IsCorrectTypeAnnotation(string text)
+    {
+        if (text == "*")
+        {
+            return true;
+        }
+        else if (text == "**kwargs" || text == "*args")
+        {
+            return false;
+        }
+        else if (Regex.IsMatch(text, @"^[^=]+=[^=]+$"))
+        {
+            return true;
+        }
+        else if (text.Contains(":"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    async Task<Dictionary<string, List<string>>> GetParamMap(IPage page, bool isClass)
+    {
+        Dictionary<string, List<string>> paramMap = new Dictionary<string, List<string>>();
+
+        IReadOnlyList<ILocator>? HTMLElementList = null;
+        if (isClass)
+        {
+            HTMLElementList = await page.Locator(".content > .wrap.has-inner-focus").AllAsync();
+        }
+        else
+        {
+            HTMLElementList = await page.Locator(".memberInfo > .wrap.has-inner-focus").AllAsync();
+        }
+
+        for (int i = 0; i < HTMLElementList.Count; i++)
+        {
+
+            var HTMLElement = HTMLElementList[i];
+            var codeText = await HTMLElement.InnerTextAsync();
+
+            var regex = new Regex(@"(?<key>.+?)\((?<params>.+?)\)");
+            var match = regex.Match(codeText);
+
+            string key = "";
+            string paramsText = "";
+
+            if (!match.Success && !isClass)
+            {
+                Console.WriteLine("Ignore codeText : ");
+                Console.WriteLine(codeText);
+                Console.WriteLine("");
+                continue;
+            }
+
+            if (!match.Success && isClass)
+            {
+                key = codeText;
+            }
+
+            if (match.Success)
+            {
+                key = match.Groups["key"].Value.Trim();
+                paramsText = match.Groups["params"].Value.Trim();
+            }
+
+            var paramList = SplitParameters(paramsText);
+
+            paramMap[key] = paramList;
+        }
+
+        return paramMap;
+    }
+
+
+
+    List<string> SplitParameters(string paramsText)
+    {
+        var paramList = new List<string>();
+        int bracketCount = 0;
+        string currentParam = "";
+
+        for (int i = 0; i < paramsText.Length; i++)
+        {
+            char c = paramsText[i];
+
+            if (c == '[')
+            {
+                bracketCount++;
+            }
+            else if (c == ']')
+            {
+                bracketCount--;
+            }
+            else if (c == ',' && bracketCount == 0)
+            {
+                paramList.Add(currentParam.Trim());
+                currentParam = "";
+                continue;
+            }
+
+            currentParam += c;
+        }
+
+        if (!string.IsNullOrWhiteSpace(currentParam))
+        {
+            paramList.Add(currentParam.Trim());
+        }
+
+        return paramList;
+    }
+
+
+    void ValidParamMap(Dictionary<string, List<string>> paramMap, bool isClass, TResult res)
+    {
+
+        foreach (var item in paramMap)
+        {
+            string key = item.Key;
+            var paramList = item.Value;
+
+            if (isClass && paramList.Count == 0)
+            {
+                res.Add($"{key}", "argument", "Class empty argument");
+            }
+
+            for (int i = 0; i < paramList.Count; i++)
+            {
+                var text = paramList[i];
+
+                if (!IsCorrectTypeAnnotation(text))
+                {
+                    res.Add($"{key}", "argument", text);
+                }
+            }
+        }
+
+    }
+
+}
+
