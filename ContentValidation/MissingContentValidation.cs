d21ec031f814b5856f2916131c618484db35da04
Zimu Li (WICRESOFT NORTH AMERICA LTD) <v-zimuli@microsoft.com>
2024-11-21 17:48:02 +0800

    Develop script GarbledTextValidation.cs, MissingContentValidation.cs and TestPageContent.cs

--- ContentValidation/MissingContentValidation.cs
+++ ContentValidation/MissingContentValidation.cs
@@ -2,7 +2,7 @@ using Microsoft.Playwright;
 
 namespace UtilityLibraries;
 
-public class MissingContentValidation: IValidation
+public class MissingContentValidation: IValidationNew
 {
     private IPlaywright _playwright;
 
@@ -11,22 +11,27 @@ public class MissingContentValidation: IValidation
         _playwright = playwright;
     }
 
-    public async Task<TResult> Validate(string testLink)
+    public async Task<TResultNew> Validate(string testLink)
     {
+        //Create a browser instance.
         var browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
         var page = await browser.NewPageAsync();
         await page.GotoAsync(testLink);
-        var res = new TResult();
+        var res = new TResultNew();
 
+        //Fetch all <table> tags and <div> tags containing data-heading-level
         var tableLocator = page.Locator("table");
         var rows = await tableLocator.Locator("tr").AllAsync();
-
         var headingDivs = await page.Locator("div.heading-wrapper[data-heading-level]").AllAsync();
-        var errorMessages = new List<string>();
+        var errorMessages = "";
+        var numbers = 0;
 
+        //Fetch all <tr> tags in the table tag.
         foreach (var row in rows)
         {
             var cells = await row.Locator("td, th").AllAsync();
+
+            //Loop through each <tr> tag and get the 'href' of the <div> tag closest to it.
             foreach (var cell in cells)
             {
                 var textContent = await cell.TextContentAsync();
@@ -34,14 +39,17 @@ public class MissingContentValidation: IValidation
                 var tdBoundingBox = await cell.BoundingBoxAsync();
                 string specificAnchorHref = string.Empty;
 
+                // Check if the bounding box is valid.
                 if (tdBoundingBox != null && tdBoundingBox.Width > 0 && tdBoundingBox.Height > 0)
                 {
                     double tdBottom = tdBoundingBox.Y + tdBoundingBox.Height;
 
+                    // Iterate through headingDivs and find the nearest <div> tag.
                     foreach (var divLocator in headingDivs)
                     {
                         var divBoundingBox = await divLocator.BoundingBoxAsync();
 
+                        // Checks if the bounding box of the current <div> tag is valid and precedes the current <p> tag.
                         if (divBoundingBox != null && divBoundingBox.Y + divBoundingBox.Height < tdBottom)
                         {
                             var anchorLocators = await divLocator.Locator("a").AllAsync();
@@ -53,15 +61,20 @@ public class MissingContentValidation: IValidation
                     }
                 }
 
+                //Check if there is an empty data cell, if so, return the nearest link.
                 if (string.IsNullOrWhiteSpace(textContent))
                 {
                     res.Result = false;
-                    var errorMessage = $"{textContent} " + $"\nLink : {testLink}+{specificAnchorHref}";
-                    errorMessages.Add(errorMessage);
+                    res.NumberOfOccurrences += 1;
+                    numbers += 1;
+                    errorMessages += $"\n{numbers}." + $" {testLink}+{specificAnchorHref}";
                 } 
             }
         }
-        res.ErrorMsg = string.Join("",errorMessages);
+        res.ErrorLink = $"\nError Link: {testLink}\n";
+        res.ErrorInfo = "Error Info: Some cells in the table are missing content.\n";
+        res.LocationsOfErrors.Add("Locations of Errors:" + errorMessages);
+
         await browser.CloseAsync();
 
         return res;
