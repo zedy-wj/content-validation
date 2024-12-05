ae1d18862f8703a43f060d087a4a5b6f998901bd
v-weiyding <v-weiyding@microsoft.com>
2024-12-05 16:09:00 +0800

    dev UnnecessarySymbolsValidation.cs

--- ContentValidation.Test/TestPageLabel.cs
+++ ContentValidation.Test/TestPageLabel.cs
@@ -12,7 +12,17 @@ namespace ContentValidation.Test
 
         static TestPageLabel()
         {
-            TestLinks = JsonSerializer.Deserialize<List<string>>(File.ReadAllText("../../../appsettings.json")) ?? new List<string>();
+            //TestLinks = JsonSerializer.Deserialize<List<string>>(File.ReadAllText("../../../appsettings.json")) ?? new List<string>();
+            TestLinks = new List<string>
+            {
+                "https://learn.microsoft.com/en-us/python/api/azure-search-documents/azure.search.documents.indexes.searchindexerclient?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/azure-search-documents/azure.search.documents.searchclient?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/azure-search-documents/azure.search.documents.searchindexingbufferedsender?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/azure-search-documents/azure.search.documents.aio.searchclient?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/azure-search-documents/azure.search.documents.aio.searchindexingbufferedsender?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/azure-search-documents/azure.search.documents.indexes.aio.searchindexerclient?view=azure-python",
+                "https://learn.microsoft.com/en-us/python/api/azure-functions-durable/azure.durable_functions.tasks?view=azure-python#azure-durable-functions-tasks-call-sub-orchestrator-task"
+            };
         }
 
         [Test]
--- ContentValidation/UnnecessarySymbolsValidation.cs
+++ ContentValidation/UnnecessarySymbolsValidation.cs
@@ -1,4 +1,5 @@
 using Microsoft.Playwright;
+using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 namespace UtilityLibraries;
@@ -7,6 +8,12 @@ public class UnnecessarySymbolsValidation : IValidation
 {
     private IPlaywright _playwright;
 
+    public  HashSet<string> valueSet = new HashSet<string>();
+
+    public  List<string> errorList = new List<string>();
+
+    public  TResult res = new TResult();
+
     public UnnecessarySymbolsValidation(IPlaywright playwright)
     {
         _playwright = playwright;
@@ -14,110 +21,26 @@ public class UnnecessarySymbolsValidation : IValidation
 
     public async Task<TResult> Validate(string testLink)
     {
-        var valueSet = new HashSet<string>();
-        var res = new TResult();
-        var errorList = new List<string>();
 
         //Create a browser instance.
         var browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
         var page = await browser.NewPageAsync();
         await page.GotoAsync(testLink);
 
-        //Fetch all 'p' tags content to store in a list. Use regular expressions to find matching target symbols.
-        var paragraphs = await page.Locator("p").AllInnerTextsAsync();
 
-        string excludePattern1 = @"(=>|-<[^>]*>)";
-        string excludePattern2 = @"\[.*?\]";
+        //Fetch all text content to store in a list. Use regular expressions to find matching unnecessary symbols.
+        string htmlContent = await GetHtmlContent(page);
+        ValidateHtmlContent(htmlContent);
 
-        if (paragraphs != null)
-        {
-            for (int i = 0; i < paragraphs.Count; i++)
-            {
-                var paragraph = paragraphs[i];
-                var paragraphMatches = Regex.Matches(paragraph, @"[\[\]<>&~]|/{3}");
-
-                foreach (Match match in paragraphMatches)
-                {
-                    if ((match.Value.Equals("<") || match.Value.Equals(">")) && Regex.IsMatch(paragraph, excludePattern1))
-                    {
-                         continue;
-                    }
-                    if ((match.Value.Equals("[") || match.Value.Equals("]")) && Regex.IsMatch(paragraph, excludePattern2))
-                    {
-                        continue;
-                    }
-                    valueSet.Add(match.Value);
-                    errorList.Add($"Paragraph no.{i + 1} contains unnecessary symbol: {match.Value} in text: {paragraph}");
-                }
-            }
-        }
-
-        var tableContents = new List<string>();
-        int index = 0;
-        var tableCount = await page.Locator("table").CountAsync();
-
-        // Fetch all 'table' content to store in a list. Use regular expressions to find the occurrence of unnecessary symbols between two closing tags.
-        for (int i = 0; i < tableCount; i++)
-        {
-            var tableContent = await page.Locator("table").Nth(i).InnerHTMLAsync();
-            tableContents.Add(tableContent);
-        }
-
-        foreach (var tableContent in tableContents)
-        {
-            var tagMatches1 = Regex.Matches(tableContent, @"<\/\w+>\s*&gt;\s*<\/\w+>");
-            var tagMatches2 = Regex.Matches(tableContent, @"<(?!\/?p\b)[^>]*>.*?~.*?<\/[^>]+>");
-
-            foreach (Match match in tagMatches1)
-            {
-                var value = match.Value;
-
-                if (!value.Equals(""))
-                {
-                    value = ">";
-                }
-
-                valueSet.Add($"{value}");
-                errorList.Add($"Table no.{index + 1} contains unnecessary symbol: {value}");
-            }
-
-            foreach (Match match in tagMatches2)
-            {
-                var value = match.Value;
-
-                if (!value.Equals(""))
-                {
-                    value = "~";
-                }
-
-                valueSet.Add($"{value}");
-                errorList.Add($"Table no.{index + 1} contains unnecessary symbol: {value}");
-            }
-
-            index++;
-        }
-
-        //Fetch all 'code' content to store in a list. Use regular expressions to find matching target symbols.
+        //Fetch all 'code' content to store in a list. Use regular expressions to find matching unnecessary symbols.
         var codeBlocks = await page.Locator("code").AllInnerTextsAsync();
+        ValidateCodeContent(codeBlocks);
 
-        if (codeBlocks != null)
-        {
-            for (int i = 0; i < codeBlocks.Count; i++)
-            {
-                var codeBlock = codeBlocks[i];
-                var tildeMatches = Regex.Matches(codeBlock, @"~");
-                
-                foreach (Match match in tildeMatches)
-                {
-                    valueSet.Add(match.Value);
-                    errorList.Add($"Code block no.{i + 1} contains unnecessary symbol: {match.Value}");
-                }
-            }
-        }
+
 
         var formattedList = errorList
             .GroupBy(item => item)
-            .Select((group, Index) => $"{Index + 1}. Appears {group.Count()} times , location : {group.Key}")
+            .Select((group, Index) => $"{Index + 1}. Appears {group.Count()} times ,  {group.Key}")
             .ToList();
 
         if (errorList.Count > 0)
@@ -132,5 +55,91 @@ public class UnnecessarySymbolsValidation : IValidation
         await browser.CloseAsync();
 
         return res;
+    }
+
+    private void ValidateCodeContent(IReadOnlyList<string> codeBlocks)
+    {
+        string includePattern = @"~";
+
+        foreach (string codeBlock in codeBlocks)
+        {
+            string[] lines = codeBlock.Split(["\r\n", "\n"], StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in lines)
+            {
+                var matchCollections = Regex.Matches(line, includePattern);
+                foreach (Match match in matchCollections)
+                {
+                    valueSet.Add(match.Value);
+                    errorList.Add($"Unnecessary symbol: {match.Value} in code: {line}");
+                }
+            }
+        }
+    }
+
+    private void ValidateHtmlContent(string htmlContent)
+    {
+        // Includes [ ] < > & ~ /// symbols
+        string includePattern = @"[\[\]<>&~]|/{3}";
+
+        // Excludes <, > used for comparison. e.g. a > b
+        string excludePattern1 = @"(?<=\w\s)[<>](?=\s\w)";
+
+        // Excludes [ ] symbols but not if there are spaces in between. e.g. list[dict] [ada dsafdasf vcxzv]
+        string excludePattern2 = @"\[(?!.*\s).*?\]";
+
+        string[] lines = htmlContent.Split(["\r\n", "\n"], StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string line in lines)
+        {
+            var matchCollections = Regex.Matches(line, includePattern);
+            foreach (Match match in matchCollections)
+            {
+                if (match.Value.Equals("<") || match.Value.Equals(">"))
+                {
+                    if (Regex.IsMatch(line, excludePattern1))
+                    {
+                        continue;
+                    }
+                    if (line.Contains("<xref"))
+                    {
+                        continue;
+                    }
+                    // Excludes => and -< -> symbols
+                    int i = match.Index - 1;
+                    if (i >= 0 && (line[i] == '=' || line[i] == '-'))
+                    {
+                        continue;
+                    }
+                }
+
+                if ((match.Value.Equals("[") || match.Value.Equals("]")))
+                {
+                    if (Regex.IsMatch(line, excludePattern2))
+                    {
+                        continue;
+                    }
+                    if (line.Contains("<xref"))
+                    {
+                        continue;
+                    }
+                }
+
+                valueSet.Add(match.Value);
+                errorList.Add($"Unnecessary symbol: {match.Value} in text: {line}");
+            }
+        }
+    }
+
+    private async Task<string> GetHtmlContent(IPage page)
+    {
+        var contentElement = page.Locator(".content");
+
+        await contentElement.EvaluateAsync(@"(element) => {
+            const codeElements = element.querySelectorAll('code');
+            codeElements.forEach(code => code.remove());
+        }");
+
+        var text = await contentElement.InnerTextAsync();
+        return text;
     }
 }
