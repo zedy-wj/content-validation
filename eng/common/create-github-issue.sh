24dc0175d344f5646f0cb02d43fd34d8db554aec
zedy <zedy@wicresoft.com>
2024-12-18 11:13:12 +0800

    If issue already exist, update the issue. If not, create a new issue.

--- eng/common/create-github-issue.sh
+++ eng/common/create-github-issue.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 set -e
 
-ServiceName=$1
-PackageName=$2
-GitHubPat=$3
-RepoOwner=$4
-RepoName=$5
+# Setting Variables
+SERVICE_NAME=$1
+PACKAGE_NAME=$2
+GITHUB_PAT=$3
+REPO_OWNER=$4
+REPO_NAME=$5
+ISSUE_TITLE="[$SERVICE_NAME - $PACKAGE_NAME] Content Validation Issue for learn microsoft website."
 
-URL="https://api.github.com/repos/$RepoOwner/$RepoName/issues"
-JSON="{
-  \"title\": \"[$ServiceName - $PackageName] Content Validation Issue for learn microsoft website.\",
-  \"body\": \"This is the body of the issue created by Azure DevOps Pipeline.\",
-  \"labels\": [\"bug\"]
-}"
+# Querying whether issue exist
+QUERY_URL="https://api.github.com/search/issues?q=repo:${REPO_OWNER}/${REPO_NAME}+is:issue+is:open+title:${ISSUE_TITLE}"
+# Getting response
+response=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" "${QUERY_URL}")
+# Parsing the response
+item_count=$(echo "$response" | jq -r '.total_count')
 
-response=$(curl -s -X POST -H "Authorization: token $GitHubPat" -H "Content-Type: application/json" -d "$JSON" "$URL")
+if [ "$item_count" -eq 0 ]; then
+  # Opening new issue
+  URL="https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues"
+  JSON="{
+    \"title\": \"$ISSUE_TITLE\",
+    \"body\": \"This is the body of the issue created by Azure DevOps Pipeline.\",
+    \"labels\": [\"bug\"]
+  }"
 
-echo "Response from GitHub API:"
-echo "$response"
+  response=$(curl -s -X POST -H "Authorization: token $GITHUB_PAT" -H "Content-Type: application/json" -d "$JSON" "$URL")
+
+  echo "Response from GitHub API:"
+  echo "$response"
+
+else
+  echo "Issue already exist."
