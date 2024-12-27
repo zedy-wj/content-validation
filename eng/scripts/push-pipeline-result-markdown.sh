83312f4b6cf7b0cbba5d8562151f52e8ec6ebe9d
Wenjie Yu（MSFT） <81678720+zedy-wj@users.noreply.github.com>
2024-12-27 15:26:43 +0800

    Wenjie dev (#72)
    * upload more file
    
    * upload total and diff issues
    
    * upload total and diff issues
    
    ---------
    
    Co-authored-by: zedy <zedy@wicresoft.com>

--- eng/scripts/push-pipeline-result-markdown.sh
+++ eng/scripts/push-pipeline-result-markdown.sh
@@ -12,26 +12,17 @@ REPO_URL="https://github.com/${REPO_OWNER}/${REPO_NAME}.git"
 CLONE_DIR="./repo-clone"
 CURRENT_DATE=$(date +"%Y-%m-%d")
 BRANCH="trigger-$CURRENT_DATE"
-VALIDATE_MD_CONTENT="
-| id | package | status | issue link | created date of issue | update date of issue |
-|---------|---------|---------|---------|---------|---------|
-| 1 | PackageA: azure-appconfiguration | PASS | IssueLink | $CURRENT_DATE | $CURRENT_DATE |
-| 2 | PackageB: azure-keyvault-keys | FAIL | IssueLink | $CURRENT_DATE | $CURRENT_DATE |
-"
+FILE_NAME="pipline-result-$CURRENT_DATE.md"
 
 git clone "https://${GITHUB_PAT}@github.com/${REPO_OWNER}/${REPO_NAME}.git" $CLONE_DIR
 cd $CLONE_DIR
 git checkout -b $BRANCH
+cp ../$FILE_NAME .
 git config --global user.email "${GIT_USER_EMAIL}"
 git config --global user.name "${GIT_USER_NAME}"
 
- 
-FILE_NAME="pipline-result-${CURRENT_DATE}.md"
-
-echo "$VALIDATE_MD_CONTENT" > "$FILE_NAME"
- 
 git add $FILE_NAME
 git commit -m "Adding $FILE_NAME with a table"
- 
+
 git remote set-url origin "https://${GITHUB_PAT}@github.com/${REPO_OWNER}/${REPO_NAME}.git"
 git push origin $BRANCH
