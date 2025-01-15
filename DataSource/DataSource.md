d6e49c4f12f3891869c664f65d0d1f48d11ffe38
Qishun Zhong (WICRESOFT NORTH AMERICA LTD) <v-qzhong@microsoft.com>
2025-01-15 17:36:07 +0800

    edit datasource

---
+++ DataSource/DataSource.md
@@ -0,0 +1,25 @@
+# DataSource
+
+## Overview
+
+Provide a brief description of the package and its purpose.
+
+## Installation
+
+Explain how to install the package. Include any prerequisites.
+
+```bash
+# Example installation command
+dotnet restore
+dotnet build
+```
+
+## Usage
+
+### How To Get PackageName
+
+- **Such as** : - loganalytics/client - mixedreality-authentication-readme
+- **Image:**
+  ![alt text](image.png)
+- **Description:**
+  You need to click into the corresponding package and find the packagename in the url between `/azure/` and `?view=azure-java-stable`
--- DataSource/Program.cs
+++ DataSource/Program.cs
@@ -3,85 +3,160 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Text.Json;
+using Microsoft.Playwright;
+using System;
 
-namespace DataSource
+namespace GetAllLink
 {
-    public class GetDataSource
+    internal class Program
     {
-        private static readonly string PYTHON_SDK_API_URL_PREFIX = "https://learn.microsoft.com/en-us/python/api";
-        private static readonly string PYTHON_SDK_API_URL_SUFFIX = "view=azure-python";
-        static void Main(string[] args)
+        private static readonly string Java_SDK_API_URL_PREFIX = "https://learn.microsoft.com/en-us/java/api/overview/azure/";
+        private static readonly string Java_SDK_API_URL_BASIC = "https://learn.microsoft.com/en-us/java/api/";
+
+        public static async Task Main(string[] args)
         {
             // Default Configuration
             using IHost host = Host.CreateApplicationBuilder(args).Build();
 
             IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
 
-            string? service = config["ServiceName"];
-            string? package = config["PackageName"];
+            string? packagename = config["PackageName"];
 
-            // Fetch all need to be validated pages in a service/packages.
             List<string> pages = new List<string>();
+            List<string> allpages = new List<string>();
 
-            pages.Add(GetServiceHomePage(service));
+            string pagelink = Java_SDK_API_URL_PREFIX + packagename;
 
-            pages.Add(GetPackagesHomePage(package));
+            await GetAllChildPage(pages, allpages, pagelink);
 
-            string apiRefDocPage = GetApiRefDocPage(package);
-
-            pages.Add(apiRefDocPage);
-
-            //Get all the pages in the package that need to be tested.
-            GetAllPages(apiRefDocPage, package, pages);
-
-            ExportData(pages);
+            ExportData(allpages);
 
             host.RunAsync();
         }
 
-        static string GetServiceHomePage(string? serviceName)
+        /// <summary>
+        /// Get all child pages of the API reference document by using `palywright`
+        /// </summary>
+        /// <param name="pages"></param>
+        /// <param name="allpages"></param>
+        /// <param name="pagelink"></param>
+        /// <returns></returns>
+        static async Task GetAllChildPage(List<string> pages, List<string> allpages, string pagelink)
         {
-            serviceName = serviceName?.ToLower().Replace(" ", "-");
-            return $"{PYTHON_SDK_API_URL_PREFIX}/overview/azure/{serviceName}?{PYTHON_SDK_API_URL_SUFFIX}";
-        }
-
-        static string GetPackagesHomePage(string? packageName) {
-            packageName = packageName?.ToLower().Replace("azure-", "");
-            return $"{PYTHON_SDK_API_URL_PREFIX}/overview/azure/{packageName}-readme?{PYTHON_SDK_API_URL_SUFFIX}";
-        }
-
-        static string GetApiRefDocPage(string? packageName)
-        {
-            return $"{PYTHON_SDK_API_URL_PREFIX}/{packageName}/{packageName?.ToLower().Replace("-",".")}?{PYTHON_SDK_API_URL_SUFFIX}";
-        }
-
-        static void GetAllPages(string apiRefDocPage, string? packageName, List<string> links)
-        {
-            HtmlWeb web = new HtmlWeb();
-            var doc = web.Load(apiRefDocPage);
-            var h1Node = doc.DocumentNode.SelectSingleNode("//h1")?.InnerText;
-
-            //The recursion terminates when there are no valid sub pages in the page or when all package links have been visited.
-            if (!string.IsNullOrEmpty(h1Node) && h1Node.Contains("Package"))
+            // Launch a browser
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
             {
-                var aNodes = doc.DocumentNode.SelectNodes("//td/a");
+                Headless = true
+            });
+
+            var page = await browser.NewPageAsync();
+            IReadOnlyList<ILocator> links = new List<ILocator>();
+
+            // Retry 5 times to get the child pages if cannot get pagelinks.
+            int i = 0;
+            while (links.Count == 0)
+            {
+                // If the page does not contain the specified content, break the loop
+                if (i == 5)
+                {
+                    break;
+                }
+
+                await page.GotoAsync(pagelink);
+
+                // Get all child pages
+                links = await page.Locator("li.tree-item.is-expanded ul.tree-group a").AllAsync();
+
+                i++;
+            }
+
+            if (links.Count != 0)
+            {
+                // Get all href attributes of the child pages
+                foreach (var link in links)
+                {
+                    var href = await link.GetAttributeAsync("href");
+                    pages.Add(href);
+                }
+
+                await browser.CloseAsync();
+
+                // Recursively get all pages of the API reference document
+                foreach (var pa in pages)
+                {
+                    allpages.Add(pa);
+                    GetAllPages(pa, allpages);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Recursively get all pages of the API reference document
+        /// </summary>
+        /// <param name="apiRefDocPage"></param>
+        /// <param name="links"></param>
+        static void GetAllPages(string apiRefDocPage, List<string> links)
+        {
+            // Load the page
+            var web = new HtmlWeb();
+            var doc = web.Load(apiRefDocPage);
+
+            // Determine if recursion is required
+            if (isTrue(apiRefDocPage))
+            {
+                // Get all a tags in method table
+                var aNodes = doc.DocumentNode.SelectNodes("//td/span/a");
 
                 if (aNodes != null)
                 {
                     foreach (var node in aNodes)
                     {
-                        string href = $"{PYTHON_SDK_API_URL_PREFIX}/{packageName}/" + node.Attributes["href"].Value;
+                        string href = Java_SDK_API_URL_BASIC + node.Attributes["href"]?.Value; 
 
+                        // Determine if the page has been contained
                         if (!links.Contains(href))
                         {
                             links.Add(href);
-
-                            //Call GetAllLinks method recursively for each new link.
-                            GetAllPages(href, packageName, links);
+                            GetAllPages(href, links);
                         }
                     }
                 }
             }
+        }
+
+        /// <summary>
+        /// Determine if recursion is required
+        /// </summary>
+        /// <param name="link">The url to be judged</param>
+        /// <returns>True or False</returns>
+        static bool isTrue(string link)
+        {
+            var web = new HtmlWeb();
+            var doc = web.Load(link);
+
+            // Determine if the page contains the specified content "Classes"
+            var h2Node = doc.DocumentNode.SelectSingleNode("//h2[@id='classes']")?.InnerText;
+            if (h2Node != null && h2Node.Contains("Classes"))
+            {
+                return true;
+            }
+
+            // Determine if the page contains the specified content "Interfaces"
+            h2Node = doc.DocumentNode.SelectSingleNode("//h2[@id='interfaces']")?.InnerText;
+            if (h2Node != null && h2Node.Contains("Interfaces"))
+            {
+                return true;
+            }
+
+            // Determine if the page contains the specified content "Enums"
+            h2Node = doc.DocumentNode.SelectSingleNode("//h2[@id='enums']")?.InnerText;
+            if (h2Node != null && h2Node.Contains("Enums"))
+            {
+                return true;
+            }
+
+            return false;
         }
 
         static void ExportData(List<string> pages)
