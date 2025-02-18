6e38f0f9dd4d4a2dc03a59fa3263fea553ee097e
Qishun Zhong (WICRESOFT NORTH AMERICA LTD) <v-qzhong@microsoft.com>
2025-02-18 17:24:37 +0800

    edit datasource for python,java,dotnet

--- DataSource/Program.cs
+++ DataSource/Program.cs
@@ -3,85 +3,158 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Text.Json;
+using Microsoft.Playwright;
 
 namespace DataSource
 {
     public class GetDataSource
     {
-        private static readonly string PYTHON_SDK_API_URL_PREFIX = "https://learn.microsoft.com/en-us/python/api";
-        private static readonly string PYTHON_SDK_API_URL_SUFFIX = "view=azure-python";
-        static void Main(string[] args)
+        private static readonly string SDK_API_URL_BASIC = "https://learn.microsoft.com/en-us/";
+        static async Task Main(string[] args)
         {
             // Default Configuration
-            using IHost host = Host.CreateApplicationBuilder(args).Build();
+            // using IHost host = Host.CreateApplicationBuilder(args).Build();
 
-            IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+            // IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
 
-            string? service = config["ServiceName"];
-            string? package = config["PackageName"];
+            string? package = "search-documents-readme";
+            string? language = "Javascript";
+            // string? package = config["PackageName"];
+            // string? language = config["Language"];
 
-            // Fetch all need to be validated pages in a service/packages.
+            string? overview_url = GetLanguagePageOverview(language);
+
             List<string> pages = new List<string>();
+            List<string> allpages = new List<string>();
+            string pagelink = $"{overview_url}/{package}";
 
-            pages.Add(GetServiceHomePage(service));
+            await GetAllChildPage(pages, allpages, pagelink);
 
-            pages.Add(GetPackagesHomePage(package));
+            foreach (var page in allpages)
+            {
+                Console.WriteLine(page);
+            }
+            Console.WriteLine(allpages.Count);
+            // ExportData(allpages);
 
-            string apiRefDocPage = GetApiRefDocPage(package);
-
-            pages.Add(apiRefDocPage);
-
-            //Get all the pages in the package that need to be tested.
-            GetAllPages(apiRefDocPage, package, pages);
-
-            ExportData(pages);
+            // host.RunAsync();
         }
 
-        static string GetServiceHomePage(string? serviceName)
+        static string GetLanguagePageOverview(string? language)
         {
-            serviceName = serviceName?.ToLower().Replace(" ", "-");
-            return $"{PYTHON_SDK_API_URL_PREFIX}/overview/azure/{serviceName}?{PYTHON_SDK_API_URL_SUFFIX}";
+            language = language?.ToLower();
+            return $"{SDK_API_URL_BASIC}/{language}/api/overview/azure/";
         }
 
-        static string GetPackagesHomePage(string? packageName) {
-            packageName = packageName?.ToLower().Replace("azure-", "");
-            return $"{PYTHON_SDK_API_URL_PREFIX}/overview/azure/{packageName}-readme?{PYTHON_SDK_API_URL_SUFFIX}";
-        }
-
-        static string GetApiRefDocPage(string? packageName)
+        static async Task GetAllChildPage(List<string> pages, List<string> allpages, string pagelink)
         {
-            return $"{PYTHON_SDK_API_URL_PREFIX}/{packageName}/{packageName?.ToLower().Replace("-",".")}?{PYTHON_SDK_API_URL_SUFFIX}";
+            // Launch a browser
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false
+            });
+
+            var page = await browser.NewPageAsync();
+            IReadOnlyList<ILocator> links = new List<ILocator>();
+
+            // Retry 5 times to get the child pages if cannot get pagelinks.
+            int i = 0;
+            while (links.Count == 0)
+            {
+                // If the page does not contain the specified content, break the loop
+                if (i == 5)
+                {
+                    break;
+                }
+
+                await page.GotoAsync(pagelink);
+
+                // Get all child pages
+                links = await page.Locator("li.tree-item.is-expanded ul.tree-group a").AllAsync();
+                
+                i++;
+            }
+
+            if (links.Count != 0)
+            {
+                // Get all href attributes of the child pages
+                foreach (var link in links)
+                {
+                    var href = await link.GetAttributeAsync("href");
+                    pages.Add(href);
+                }
+
+                // await browser.CloseAsync();
+
+                // Recursively get all pages of the API reference document
+                foreach (var pa in pages)
+                {
+                    int lastSlashIndex = pa.LastIndexOf('/');
+                    string baseUri = pa.Substring(0, lastSlashIndex + 1);
+                    allpages.Add(pa);
+                    GetAllPages(pa, baseUri, allpages);
+                }
+            }
         }
 
-        static void GetAllPages(string apiRefDocPage, string? packageName, List<string> links)
+        static void GetAllPages(string apiRefDocPage, string? baseUri, List<string> links)
         {
             HtmlWeb web = new HtmlWeb();
             var doc = web.Load(apiRefDocPage);
-            var h1Node = doc.DocumentNode.SelectSingleNode("//h1")?.InnerText;
 
             //The recursion terminates when there are no valid sub pages in the page or when all package links have been visited.
-            if (!string.IsNullOrEmpty(h1Node) && h1Node.Contains("Package"))
+            if (IsTrue(apiRefDocPage))
             {
-                var aNodes = doc.DocumentNode.SelectNodes("//td/a");
+                // Console.WriteLine("今天天气不错，适合散步");
+                var aNodes = doc.DocumentNode.SelectNodes("//td/a | //td/span/a");
 
                 if (aNodes != null)
                 {
+                    // Console.WriteLine("今天天气不错，适合散步");
                     foreach (var node in aNodes)
                     {
-                        string href = $"{PYTHON_SDK_API_URL_PREFIX}/{packageName}/" + node.Attributes["href"].Value;
+                        string href = $"{baseUri}/" + node.Attributes["href"].Value;
 
                         if (!links.Contains(href))
                         {
                             links.Add(href);
 
                             //Call GetAllLinks method recursively for each new link.
-                            GetAllPages(href, packageName, links);
+                            GetAllPages(href, baseUri, links);
                         }
                     }
                 }
             }
         }
 
+        static bool IsTrue(string link)
+        {
+            var web = new HtmlWeb();
+            var doc = web.Load(link);
+            var checks = new[]
+            {
+                new { XPath = "//h1", Content = "Package" },
+                new { XPath = "//h2[@id='classes']", Content = "Classes" },
+                new { XPath = "//h2[@id='interfaces']", Content = "Interfaces" },
+                new { XPath = "//h2[@id='structs']", Content = "Structs" },
+                new { XPath = "//h2[@id='typeAliases']", Content = "Type Aliases" },
+                new { XPath = "//h2[@id='functions']", Content = "Functions" },
+                new { XPath = "//h2[@id='enums']", Content = "Enums" }
+            };
+
+            foreach (var check in checks)
+            {
+                string? hNode = doc.DocumentNode.SelectSingleNode(check.XPath)?.InnerText;
+                if (!string.IsNullOrEmpty(hNode) && hNode.Contains(check.Content))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
         static void ExportData(List<string> pages)
         {
             string jsonString = JsonSerializer.Serialize(pages);
