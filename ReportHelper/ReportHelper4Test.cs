7ab2c60ce5606793200ee4441c567451af560388
v-qzhong <v-qzhong@microsoft.com>
2025-05-21 13:00:48 +0800

    test autosize

--- ReportHelper/ReportHelper4Test.cs
+++ ReportHelper/ReportHelper4Test.cs
@@ -129,10 +129,10 @@ public class ExcelHelper4Test
                 }
 
                 // Automatically adjust column widths to fit content
-                for (int col = 0; col < sheet.GetRow(0).LastCellNum; col++)
-                {
-                    sheet.AutoSizeColumn(col);
-                }
+                // for (int col = 0; col < sheet.GetRow(0).LastCellNum; col++)
+                // {
+                //     sheet.AutoSizeColumn(col);
+                // }
 
                 // Save the updated workbook back to the file
                 using (var outFs = new FileStream(localFilePath, FileMode.Create, FileAccess.Write))
