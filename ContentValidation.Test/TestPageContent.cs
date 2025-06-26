0776183492dac20ab32b3d3320b6c87019a6c887
v-qzhong <v-qzhong@microsoft.com>
2025-06-26 10:05:40 +0800

    filter dotnet test rules

--- ContentValidation.Test/TestPageContent.cs
+++ ContentValidation.Test/TestPageContent.cs
@@ -66,7 +66,7 @@ namespace ContentValidation.Test
         [Category("PythonTest")]
         [Category("JavaTest")]
         [Category("JsTest")]
-        [Category("DotNetTest")]
+        // [Category("DotNetTest")]
         [TestCaseSource(nameof(TestLinks))]
         public async Task TestTableMissingContent(string testLink)
         {
